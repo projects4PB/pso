46fe3c878a8075c6120266cadbe4d0ded26cf24a
darkon <darkon@Darkonlp>
2014-05-31 13:30:39 +0200

    poczatek randoma, przypisane stale

---
+++ pso/src/pso.h
@@ -0,0 +1,22 @@
+/*
+ * pso.h
+ *
+ *  Created on: 31 maj 2014
+ *      Author: darkon
+ */
+
+#ifndef PSO_H_
+#define PSO_H_
+
+
+
+int dim=2;
+int size=8;
+int c1=2;
+int c2=2;
+int maxSpeed=60;
+int iterations=1;
+double seed=1;
+
+
+#endif /* PSO_H_ */
---
+++ pso/src/random.cpp
@@ -0,0 +1,23 @@
+/*
+ * random.cpp
+ *
+ *  Created on: 31 maj 2014
+ *      Author: darkon
+ */
+
+
+
+public random() {
+	double seed=1;
+
+
+
+
+public random() {
+}
+
+public random(double seed) {
+
+}
+
+}
